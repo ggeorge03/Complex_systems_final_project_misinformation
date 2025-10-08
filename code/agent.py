8f3cfec6dd6d67c4967fa28f79c1bf0bbcaddd2b
George Guillain <georgeguillain@gmail.com>
2025-10-08 15:06:39 +0800

    bug fixes to get the model running in Solara

--- code/agent.py
+++ code/agent.py
@@ -1,21 +1,18 @@
+import numpy as np
 from mesa import Agent
 import random
 
 class PersonAgent(Agent):
-    """A simple agent that can be Susceptible (S) or Infected (I)."""
-    def __init__(self, unique_id, model, state="S"):
-        super().__init__(unique_id, model)
+    """Agent representing a person who can be Susceptible (S) or Infected (I)."""
+    
+    def __init__(self, model, state="S"):
+        super().__init__(model)  # **model is required!**
         self.state = state
 
     def step(self):
+        """Agent tries to infect nearby susceptible agents if infected."""
         if self.state == "I":
-            # Try recovery
-            if random.random() < self.model.gamma:
-                self.state = "S"
-            else:
-                # Try to infect a neighbour
-                neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False)
-                if neighbors:
-                    other = random.choice(neighbors)
-                    if other.state == "S" and random.random() < self.model.beta:
-                        other.state = "I"
+            neighbors = self.model.space.get_neighbors(self.pos, radius=5, include_center=False)
+            for other in neighbors:
+                if other.state == "S" and random.random() < self.model.beta:
+                    other.state = "I"
